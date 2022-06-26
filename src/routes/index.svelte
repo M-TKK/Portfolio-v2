0e957c15c061c9b1af64041d4d6c6987f9ccbf19
MTK <51107821+M-TKK@users.noreply.github.com>
2022-06-27 02:41:58 +1000

    commit to main

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 <Profile/>
 
 <div class="bg-[#e05f5f]">
-    <video src="https://www.dropbox.com/s/49g7vtwkysny1m5/bg.mp4?raw=1" class="absolute -top-0 left-0 object-cover w-screen h-screen object-center opacity-40 z-1" autoplay loop preload="auto">
+    <video src="https://www.dropbox.com/s/49g7vtwkysny1m5/bg.mp4?raw=1" class="absolute -top-0 left-0 object-cover w-screen h-screen object-center opacity-40 z-1" autoplay loop playsinline muted>
         <track kind="captions">
     </video>
 </div>
